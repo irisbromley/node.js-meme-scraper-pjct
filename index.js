8def8b159c2b7181d287484472e9e1b0e2c2e7cc
Iris Bromley <iris.j.bromley@gmail.com>
2023-01-11 22:12:58 +0100

    Fetch html (boring message)

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('This is it!');
+
+let url = 'https://memegen-link-examples-upleveled.netlify.app/';
+console.log(url);
+
+
+
+// Below fetch returns html of the url Wohoo
+fetch(url)
+  .then((response) => response.text())
+  .then((cabbage) => console.log(cabbage))
+  .catch((e) => console.log(e));
+// next I want to get the first 10 img from above html
