01a23eb7e7bc9dcd442c43aa11427676cb954624
Iris Bromley <iris.j.bromley@gmail.com>
2023-01-12 15:22:14 +0100

    Add cheerio npm package for html parsing

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 console.log('This is it!');
 
-let url = 'https://memegen-link-examples-upleveled.netlify.app/';
-console.log(url);
-
-
-
-// Below fetch returns html of the url Wohoo
-fetch(url)
+let htmlContent = fetch('https://memegen-link-examples-upleveled.netlify.app/')
   .then((response) => response.text())
-  .then((cabbage) => console.log(cabbage))
-  .catch((e) => console.log(e));
-// next I want to get the first 10 img from above html
+  .then((data) => console.log(data));
+
+// // Below fetch returns html of the url Wohoo
+// fetch(url).then((response) => console.log(response.text()));
+// if (response.text()) console.log(response.text()).catch((e) => console.log(e));
+
+// // next I want to get the first 1 img from above html
