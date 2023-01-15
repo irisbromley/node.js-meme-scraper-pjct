5064571e7161069bd415956b0ad1616a7700d70a
Iris Bromley <iris.j.bromley@gmail.com>
2023-01-15 15:45:22 +0100

    Remove unused code

--- index.js
+++ index.js
@@ -69,25 +69,3 @@ function makeFileName(i) {
   }
   return filename;
 }
-// console.log(makeFileName(10));
-
-//use native code to download the images
-// function downloadImage(url, filepath) {
-//   return new Promise((resolve, reject) => {
-//     client.get(url, (res) => {
-//       if (res.statusCode === 200) {
-//         res
-//           .pipe(fs.createWriteStream(filepath))
-//           .on('error', reject)
-//           .once('close', () => resolve(filepath));
-//       } else {
-//         // Consume response data to free up memory
-//         res.resume();
-//         reject(
-//           new Error(`Request Failed With a Status Code: ${res.statusCode}`),
-//         );
-//       }
-//     });
-//   });
-// }
-// console.log(downloadImage());
